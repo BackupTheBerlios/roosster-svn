a41baff7fe33d072b25e121dc25862cc6cefb974
firstbman <firstbman@8a4ffc04-99ea-0310-a765-abcd940ec813>
2005-06-17 09:55:19 +0000

    added GuiConstants.java

---
+++ trunk/src/org/roosster/gui/GuiConstants.java
@@ -0,0 +1,53 @@
+/*
+ * This file is part of ROOSSTER.
+ * Copyright 2004, Benjamin Reitzammer <benjamin@roosster.org>
+ * All rights reserved.
+ *
+ * ROOSSTER is free software; you can redistribute it and/or modify
+ * it under the terms of the Artistic License.
+ *
+ * You should have received a copy of the Artistic License
+ * along with ROOSSTER; if not, go to
+ * http://www.opensource.org/licenses/artistic-license.php for details
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.roosster.main;
+
+public interface GuiConstants 
+{
+    public static final int SEARCH_TAB_INDEX    = 0;
+    public static final int EDIT_TAB_INDEX      = 1;
+
+    public static final String TABBED_PANE          = "tabbedPane";
+    public static final String EDIT_TAB             = "edidtTab";
+    public static final String SEARCH_TAB           = "searchTab";
+    
+    public static final String URL_FIELD            = "urlField";
+    public static final String TITLE_FIELD          = "titleField";
+    public static final String TAGS_FIELD           = "tagsField";
+    public static final String NOTE_FIELD           = "noteField";
+    public static final String TYPE_FIELD           = "typeField";
+    public static final String AUTHOR_FIELD         = "authorField";
+    public static final String AUTHOREMAIL_FIELD    = "authoremailField";
+
+    public static final String QUERY_FIELD          = "queryField";
+
+    public static final String SEARCH_RESULT        = "searchresultTable";
+
+
+}
+
+
